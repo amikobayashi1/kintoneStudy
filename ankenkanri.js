dd47b3c37bbf5de09adc70cd8cc2ba378a33a09c
tsuneyama <akiko.tsuneyama@asnet.co.jp>
2024-07-02 14:01:17 +0900

    案件管理js 新規追加

---
+++ ankenkanri.js
@@ -0,0 +1,25 @@
+(() => {
+    "use strict";
+
+    const ANKEN_FIELD_CODE = 'cf_案件ID';
+    const KOKYAKU_FIELD_CODE = 'cf_顧客ID';
+
+    kintone.events.on([
+        'app.record.create.show',
+        'app.record.edit.show',
+        'app.record.index.edit.show'
+    ], (event) => {
+
+        // 顧客IDに値が入っていれば自動ルックアップ
+        if(event.record[KOKYAKU_FIELD_CODE].value) {
+            event.record[KOKYAKU_FIELD_CODE]["lookup"] = true;
+        }
+
+        // 案件IDは自動採番なので、編集不可にする
+        event.record[ANKEN_FIELD_CODE].disabled = true;
+
+        return event;
+
+    });
+
+})();
