7db36c1a2b8edb908d3f81592e0d54c5718b8c29
tsuneyama <akiko.tsuneyama@asnet.co.jp>
2024-07-02 17:46:08 +0900

    受注見積ID追加

--- ankenkanri.js
+++ ankenkanri.js
@@ -3,6 +3,7 @@
 
     const ANKEN_FIELD_CODE = 'cf_案件ID';
     const KOKYAKU_FIELD_CODE = 'cf_顧客ID';
+    const MITSUMORI_FIELD_CODE = 'cf_受注見積ID';
 
     kintone.events.on([
         'app.record.create.show',
@@ -15,8 +16,12 @@
             event.record[KOKYAKU_FIELD_CODE]["lookup"] = true;
         }
 
-        // 案件IDは自動採番なので、編集不可にする
+        // 案件IDは自動採番なので、編集不可
         event.record[ANKEN_FIELD_CODE].disabled = true;
+
+        // 受注見積IDは見積管理アプリのステータスが受注に変更、保存された場合に自動入力されるため、編集不可
+        event.record[MITSUMORI_FIELD_CODE].disabled = true;
+
 
         return event;
 
